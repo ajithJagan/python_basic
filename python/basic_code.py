4525d8b7d1e57302dafe12bc78c4404c12e9b17c
ajith <ajith@krestwolf.ai>
2024-12-28 12:22:59 +0530

    Basics codes

---
+++ python/basic_code.py
@@ -0,0 +1,12 @@
+# import keyword
+
+# print(keyword.kwlist)
+# print("Enter your name ")
+name = input("Enter your name ")
+print("Your name is = ",name)
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+c = a+b
+print("Total number is : ",c)
